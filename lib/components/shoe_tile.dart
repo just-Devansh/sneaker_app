183eca973ea2cab76b8eeae6c5361acdc5e05e86
Devansh <just.devansh0201@gmail.com>
2024-06-09 10:43:58 +0530

    shop page completed

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_app/models/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 25.0),
+      width: 300,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // shoe picture
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              shoe.imagePath,
+              height: 300,
+              width: 280,
+            ),
+          ),
+
+          // description
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Text(
+              shoe.description,
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          // price + details
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // shoe name
+                      Text(
+                        shoe.shoeName,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      // shoe price
+                      Text(
+                        '\$${shoe.price}',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // add to cart icon
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          )
+
+          // button to add to cart
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneaker_app/components/shoe_tile.dart';
+import 'package:sneaker_app/models/cart.dart';
+import 'package:sneaker_app/models/shoe.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,10 +16,90 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Center(
-        child: Text('S H O P   P A G E'),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        children: [
+          // search bar
+          Container(
+            padding: EdgeInsets.all(12.0),
+            margin: EdgeInsets.symmetric(horizontal: 25.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(color: Colors.grey[600]),
+                ),
+                Icon(
+                  Icons.search_rounded,
+                  color: Colors.grey[600],
+                ),
+              ],
+            ),
+          ),
+
+          // message
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+              'everyone flies.. some fly longer than others',
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+
+          // hot picks
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hot Picks 🔥',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                // get a shoe from shop list
+                Shoe shoe = value.getShoeList()[index];
+
+                // return the shoe
+                return ShoeTile(shoe: shoe);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
+            child: Divider(
+              color: Colors.grey[100],
+            ),
+          ),
+        ],
       ),
     );
   }
